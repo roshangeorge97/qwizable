6d7b0e30af6815ebb0dd656c6fe39cf64af2e337
Roshan George <roshangeorge2003@gmail.com>
2024-08-04 15:32:35 +0530

    deploy bug fix4

--- client/src/firebase.jsx
+++ client/src/firebase.jsx
@@ -6,16 +6,19 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCcw1l425lbNLT2bLQ-MHzBW_hYdlZLfqY",
+  authDomain: "qwizable-a9399.firebaseapp.com",
+  projectId: "qwizable-a9399",
+  storageBucket: "qwizable-a9399.appspot.com",
+  messagingSenderId: "630923548842",
+  appId: "1:630923548842:web:dd6fc6339e7f5bac5e98d3",
+  measurementId: "G-G47N4DV7CS"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
